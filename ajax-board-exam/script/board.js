6480acdf82798b7a1937b1f1beb356ce1f9696d4
anveloper <90117593+anveloper@users.noreply.github.com>
2022-04-26 12:51:56 +0900

    Add files via upload

---
+++ ajax-board-exam/script/board.js
@@ -0,0 +1,27 @@
+function getUsers() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+                const users = JSON.parse(xhr.responseText);
+                // console.log(typeof users);
+
+                let tbodyHTML = "";
+                users.forEach(ele => {
+                    // console.dir(ele)
+                    tbodyHTML += `
+                        <tr>
+                            <td>${ele.id}</td>
+                            <td>${ele.name}</td>
+                            <td>${ele.phone}</td>
+                            <td>${ele.website}</td>
+                        </tr>
+                    `;
+                });
+                document.querySelector("#boardTbl > tbody").innerHTML = tbodyHTML;
+            }
+        }
+    };
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+};
